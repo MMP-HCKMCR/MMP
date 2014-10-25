067326edcdd9c8d7a67756d2df395e6848588c71
Christopher Jones <cj2978@me.com>
2014-10-25 22:47:07 +0100

    EventTypeRepository added

---
+++ MMP.HackMCR.DataAccess/Objects/EventType.cs
@@ -0,0 +1,9 @@
+﻿namespace MMP.HackMCR.DataAccess.Objects
+{
+    public class EventType
+    {
+        public int EventTypeId { set; get; }
+        
+        public string EventTypeName { set; get; }     
+    }
+}
---
+++ MMP.HackMCR.DataAccess/Repositories/EventTypeRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using MMP.HackMCR.DataAccess.Objects;
+
+namespace MMP.HackMCR.DataAccess.Repositories
+{
+    public class EventTypeRepository
+    {
+        public EventType AddEventType(string eventTypeName)
+        {
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter("@EventTypeName", eventTypeName)
+            };
+
+            return PopulateEventTypeFromDataTable(DataHelper.PopulateTable("sp_AddEventType", parameters));
+        }
+
+        private static EventType PopulateEventTypeFromDataTable(DataTable dataTable)
+        {
+            return dataTable.Rows.Count > 0 ? PopulateEventTypeFromDataRow(dataTable.Rows[0]) : null;
+        }
+
+        private static List<EventType> PopulateEventTypesFromDataTable(DataTable dataTable)
+        {
+            return (from DataRow dataRow in dataTable.Rows select PopulateEventTypeFromDataRow(dataRow)).ToList();
+        }
+
+        private static EventType PopulateEventTypeFromDataRow(DataRow dataRow)
+        {
+            return new EventType
+            {
+                EventTypeId = dataRow.Field<int>("EventTypeId"),
+                EventTypeName = dataRow.Field<string>("EventTypeName")
+            };
+        }
+    }
+}
