1345eb068e5154932e7967a9599b9d2906ddb659
Kirsty Hunter <kirst2k@hotmail.com>
2014-10-26 08:42:13 +0000

    Remove & Add OneDiary Entry calls

--- MMP.HackMCR.OneDiaryInterface/Inferface.cs
+++ MMP.HackMCR.OneDiaryInterface/Inferface.cs
@@ -3,6 +3,9 @@ using System.Net;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using MMP.HackMCR.DataContract.Objects;
+using System.Collections.Generic;
+using MMP.HackMCR.DataContract;
+using System.IO;
 
 namespace MMP.HackMCR.OneDiaryInterface
 {
@@ -32,5 +35,43 @@ namespace MMP.HackMCR.OneDiaryInterface
 
             return entries;
         }
+
+        public static void AddCalanderEntry(string userToken, List<User> users, Entry entry)
+        {
+            foreach(User user in users)
+            {
+                var url = new StringBuilder("https://api.onediary.com/v1/calendars/");
+                url.Append(user.Entries.events[0].calendar_id);
+                url.Append("/events");
+
+                var webRequest = WebRequest.Create(url.ToString());
+                webRequest.Headers.Add("Authorization", string.Format("Bearer {0}", userToken));
+
+                entry.calendar_id = user.Entries.events[0].calendar_id;
+
+                MemoryStream stream1 = new MemoryStream();
+                var serializer = new DataContractJsonSerializer(typeof(Entry));
+                serializer.WriteObject(stream1, entry);
+            }
+        }
+
+        public static void RemoveCalanderEntry(string userToken, List<User> users, int eventId)
+        {
+            foreach (User user in users)
+            {
+                var url = new StringBuilder("https://api.onediary.com/v1/calendars/");
+                url.Append(user.Entries.events[0].calendar_id);
+                url.Append("/events");
+
+                var webRequest = WebRequest.Create(url.ToString());
+                webRequest.Headers.Add("Authorization", string.Format("Bearer {0}", userToken));
+
+                MemoryStream stream1 = new MemoryStream();
+                var serializer = new DataContractJsonSerializer(typeof(Entry));
+                serializer.WriteObject(stream1, eventId);
+            }
+        }
+
+
     }
 }
